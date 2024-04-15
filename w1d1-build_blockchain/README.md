e11a8e19dc8207fd58df800dd3135deaebb35ad4
zhtkeepup <zhtkeepup@gmail.com>
2024-04-16 02:38:28 +0800

    补充共识算法

--- w1d1-build_blockchain/README.md
+++ w1d1-build_blockchain/README.md
@@ -1,4 +1,4 @@
-用 Python 从零开始创建区块链.（单链）
+用 Python 从零开始创建区块链.
 
 ```
 https://learnblockchain.cn/2017/10/27/build_blockchain_by_python/
@@ -27,3 +27,91 @@ https://learnblockchain.cn/2017/10/27/build_blockchain_by_python/
 ### get 接口查询链信息
 
 ![链信息](./resource/03chain-info.png "链信息")
+
+## --
+
+### 多个端口模拟多节点网络，观察共识算法。
+
+```
+# 多节点启动命令:
+python3 blockchain.py 5000
+python3 blockchain.py 5001
+```
+
+1. post 方式调用`http://localhost:5000/nodes/register` 向节点 1 注册节点 2 的信息，请求体为:
+
+   ```
+    {
+        "nodes": ["http://localhost:5001"]
+    }
+   ```
+
+   当前场景只观察节点 1 上的实现效果，故略去向节点 2 注册节点 1 的步骤。
+
+1. 调用`http://localhost:5000/mine` 两次在节点 1 上生成两个区块。
+1. 调用`http://localhost:5001/mine` 两次在节点 2 上生成三个区块。
+1. 向节点 1 上调用`http://localhost:5000/nodes/resolve` ，观察共识算法。
+
+--
+
+下面截图展示调用 resolve 之前 chain 的查询信息，以及 resolve 的效果。
+
+![resove之前的chain信息](./resource/04chain-before-resolve.png "resove之前的chain信息")
+
+![resolve请求的效果](./resource/05resove-1.png "resolve请求的效果")
+
+resove 接口返回体信息:
+
+```
+{
+    "message": "Our chain was replaced",
+    "new_chain": [
+        {
+            "index": 1,
+            "previous_hash": 1,
+            "proof": 100,
+            "timestamp": 1713205842.8947453,
+            "transactions": []
+        },
+        {
+            "index": 2,
+            "previous_hash": "399aafc5a8a2b10a516208f699a46c4346b7ba98a46adc567d3281d27e1cea73",
+            "proof": 35293,
+            "timestamp": 1713205939.5651655,
+            "transactions": [
+                {
+                    "amount": 1,
+                    "recipient": "d52ae15cb7df4fe0a6d3ffb57b7734dc",
+                    "sender": "0"
+                }
+            ]
+        },
+        {
+            "index": 3,
+            "previous_hash": "16812f954e80175b4999f40aefbc79109d199848d2c6a7310481ca2a3b6eb669",
+            "proof": 35089,
+            "timestamp": 1713205940.6716533,
+            "transactions": [
+                {
+                    "amount": 1,
+                    "recipient": "d52ae15cb7df4fe0a6d3ffb57b7734dc",
+                    "sender": "0"
+                }
+            ]
+        },
+        {
+            "index": 4,
+            "previous_hash": "d494cf3fe8341b75b6321510f1087592ca1c7d3fcc9c797fecc615a435f997e1",
+            "proof": 119678,
+            "timestamp": 1713205942.1483912,
+            "transactions": [
+                {
+                    "amount": 1,
+                    "recipient": "d52ae15cb7df4fe0a6d3ffb57b7734dc",
+                    "sender": "0"
+                }
+            ]
+        }
+    ]
+}
+```
--- w1d1-build_blockchain/blockchain.py
+++ w1d1-build_blockchain/blockchain.py
@@ -8,6 +8,11 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+import requests
+
+import sys
+
+from urllib.parse import parse_qsl, urljoin, urlparse
 
 class Blockchain(object):
     def __init__(self):
@@ -16,6 +21,84 @@ class Blockchain(object):
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+        self.nodes = set()
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        共识算法解决冲突
+        使用网络中最长的链.
+        :return: <bool> True 如果链被取代, 否则为False
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
+
+
 
     # 创建新区块
     def new_block(self, proof, previous_hash=None):
@@ -175,5 +258,51 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
+
+
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 5000
+    app.run(host='0.0.0.0', port=port)
