921e99e5f5430ce1e295c24d7430ba5ad7eb1c86
zhtkeepup <zhtkeepup@gmail.com>
2024-04-30 23:52:49 +0800

    第一个设thegraph

--- w3d2-thegrapth/thegraph/thegraph/src/nft-factory-0-x-9-e-72.ts
+++ w3d2-thegrapth/thegraph/thegraph/src/nft-factory-0-x-9-e-72.ts
@@ -1,17 +1,23 @@
 import {
   NFTCreated as NFTCreatedEvent,
   NFTRegesitered as NFTRegesiteredEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
 } from "../generated/NFTFactory0x9e72/NFTFactory0x9e72"
 import {
   NFTCreated,
   NFTRegesitered,
-  OwnershipTransferred
+  OwnershipTransferred,
+  TokenInfo,
+  Transfer
 } from "../generated/schema"
+
+import { S2NFT } from "../generated/templates/S2NFT/S2NFT"
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   let entity = new NFTCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    event.params.nftCA
+    // event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.nftCA = event.params.nftCA
 
@@ -50,3 +56,30 @@ export function handleOwnershipTransferred(
 
   entity.save()
 }
+
+export function handleTransfer(
+  event: TransferEvent
+): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+  entity.save()
+
+  let entityNFT = NFTCreated.load(event.address);
+
+  if(entityNFT != null) {
+    let contract = S2NFT.bind(event.address)
+    let tokenInfo = new TokenInfo(entityNFT.nftCA.toHexString().concat("#").concat(entity.tokenId.toString()));
+    tokenInfo.ca = entityNFT.nftCA;
+    tokenInfo.tokenId = entity.tokenId;
+    tokenInfo.tokenURL = contract.tokenURI(tokenInfo.tokenId);
+    tokenInfo.name = contract.name();
+    tokenInfo.owner = entity.to;
+    tokenInfo.save();
+  }
+
+}
+
