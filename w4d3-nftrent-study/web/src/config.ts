a8e75629c495b16b168e0f377564d3a2b373d026
zhtkeepup <zhtkeepup@gmail.com>
2024-05-09 01:29:21 +0800

    111

--- w4d3-nftrent-study/web/src/config.ts
+++ w4d3-nftrent-study/web/src/config.ts
@@ -44,8 +44,12 @@ import { http, createConfig } from "@wagmi/core";
 export const wagmiConfig = createConfig({
   chains: [mainnet, sepolia],
   transports: {
-    [mainnet.id]: http("https://ethereum-rpc.publicnode.com"),
-    [sepolia.id]: http("https://ethereum-sepolia-rpc.publicnode.com"),
+    [mainnet.id]: http(
+      "https://eth-mainnet.g.alchemy.com/v2/jrvBMymLzPjSOTt6IoKelzCelQ4dRQGs"
+    ),
+    [sepolia.id]: http(
+      "https://eth-sepolia.g.alchemy.com/v2/jrvBMymLzPjSOTt6IoKelzCelQ4dRQGs"
+    ),
   },
 });
 
